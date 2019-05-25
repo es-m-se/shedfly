75d0c56521aadcf61f32507c7f72b31817951eab
shan <smc@shedfly.com>
2019-05-25 16:16:16 +1000

    testing 123

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
-    return ['<!DOCTYPE html><html><meta charset="utf-8"><title>It works',
-            '</title><h1>It works!</h1>'] 
+    return ['<!DOCTYPE html><html><meta charset="utf-8"><title>Shedfly',
+            '</title><h1>Testing123</h1>']
