6d45d8dac12e70868c10d996f17dc5b6364bcf7c
shan <smc@shedfly.com>
2019-06-15 19:40:19 +1000

    attempt 3 to correctly server static files

--- shedfly/__init__.py
+++ shedfly/__init__.py
@@ -4,9 +4,7 @@ from flask import Flask
 
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
-    app = Flask(__name__, static_folder='static', static_url_path="/static")
-    #app = Flask(__name__)
-
+    app = Flask(__name__, static_folder='static_content', static_url_path="/static_content")
     app.register_blueprint(web_app.bp)
 
     return app
