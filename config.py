6fcc69f900a316853b59e945d0860c5c12b3368c
shan <smc@shedfly.com>
2019-06-20 13:37:08 +1000

    attempt 7 to correctly serve static files

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
 
 class Config():
-    GPAAS_DIR = os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(__file__))))
-    STATIC_URL = '/static/'
+    GPAAS_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_URL = '/shedfly/static/'
     STATIC_ROOT = os.path.join(GPAAS_DIR, 'static')
-    MEDIA_URL = '/media/'
-    MEDIA_ROOT = os.path.join(GPAAS_DIR, 'media')
